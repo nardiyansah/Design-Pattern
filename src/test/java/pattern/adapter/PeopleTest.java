c86adca946eb4e9a8fadee4172e53aab5b2190bc
nardiyansah <janatri1398@gmail.com>
2022-06-01 10:37:42 +0700

    Change type to interface

--- src/test/java/pattern/adapter/PeopleTest.java
+++ src/test/java/pattern/adapter/PeopleTest.java
@@ -6,14 +6,14 @@ class PeopleTest {
 
     @org.junit.jupiter.api.Test
     void eat() {
-        People people = new People();
+        HumanInterface people = new People();
 
         assertEquals("This creature need eat", people.eat());
     }
 
     @org.junit.jupiter.api.Test
     void die() {
-        People people = new People();
+        HumanInterface people = new People();
 
         assertEquals("This creature will die", people.die());
     }
